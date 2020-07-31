8eba1711f62de513329dd65d75b1b472d8c9be74
Ivan Holub <ivanholub.js@gmail.com>
2020-07-31 16:50:02 +0300

    feat: add 2 init components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+
+    state = {};
+
+    render(){
+        return(
+            <div>
+                <form action="index.html" method="POST">
+                    <input type="text"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+
+export default SearchBar;
